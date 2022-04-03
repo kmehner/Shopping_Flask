f49025a45349d259fb7acebac0744ac96fa71945
Katelyn Mehner <kmehner@gmail.com>
2022-04-03 16:05:04 -0600

    Cart routes

--- app/blueprints/cart/routes.py
+++ app/blueprints/cart/routes.py
@@ -11,10 +11,10 @@ def index():
     return render_template('index.html', title=title, items=items)
 
 
-@cart.route('/create-post', methods=['GET', 'POST'])
+@cart.route('/add-product', methods=['GET', 'POST'])
 @login_required
-def create_post():
-    title = 'Create A Post'
+def add_product():
+    title = 'Add a Product'
     form = ItemForm()
     if form.validate_on_submit():
         title = form.title.data
@@ -23,3 +23,30 @@ def create_post():
         flash(f"{new_item.title} has been created", 'secondary')
         return redirect(url_for('cart.index'))
     return render_template('add_products.html', title=title, form=form)
+
+
+@cart.route('/my-items')
+@login_required
+def my_items():
+    title = 'My Items'
+    items = current_user.cart_items.all()
+    return render_template('my_items.html', title=title, items=items)
+
+# Get A Single Item by ID
+@cart.route('/items/<item_id>')
+@login_required
+def single_iteem(item_id):
+    item = Item.query.get_or_404(item_id)
+    title = item.title
+    return render_template('post_detail.html', title=title, item=item)
+
+# Get all items that match search
+@cart.route('/search-iteems', methods=['GET', 'POST'])
+def search_items():
+    title = 'Search'
+    form = SearchForm()
+    items = []
+    if form.validate_on_submit():
+        term = form.search.data
+        items = Item.query.filter( ( Item.title.ilike(f'%{term}%')) | ( Item.body.ilike(f'%{term}%')) ).all()
+    return render_template('search_items.html', title=title, items=items, form=form)
